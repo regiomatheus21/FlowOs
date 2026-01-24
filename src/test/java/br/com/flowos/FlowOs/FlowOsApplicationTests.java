12e5141cf358dbb21aae8e1b8578ea881e9aa07b
regio.matheus <regio.delima@socin.com.br>
2026-01-24 20:49:33 -0300

    cria projeto

---
+++ src/test/java/br/com/flowos/FlowOs/FlowOsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flowos.FlowOs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
